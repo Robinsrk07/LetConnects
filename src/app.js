a932dec1d06c51d8b3522c30ac34cb03f238a84f
Robinsrk07 <robinsyriak07@gmail.com>
2024-12-13 11:36:42 +0530

    params

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require('express')
-
 const app= express()
 
+app.get('/ab*c',(req,res)=>{
+    res.send('****')
+})
+app.get('/test/:userId/:name',(req,res)=>{
+    res.send('ostman')
+} )
 
+app.post('/user',(req,res)=>{
+    console.log("save data from database");
+    res.send('data succesfully saved')
+    
+})
+app.use('/hello',(req,res)=>{
+    res.send('hello')
+})
+ 
+// app.use('/',(req,res)=>{
+//     res.send('landing')
+// })
 app.listen(3000)
+  
