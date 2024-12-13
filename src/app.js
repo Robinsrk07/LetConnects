297049616bdda4c604947b7f01ad274f78afb3cc
Robinsrk07 <robinsyriak07@gmail.com>
2024-12-13 12:43:33 +0530

    multiple routehandler

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app= express()
 
-app.get('/ab*c',(req,res)=>{
-    res.send('****')
-})
-app.get('/test/:userId/:name',(req,res)=>{
-    res.send('ostman')
+app.get('/ab*c',(req,res,next)=>{
+    console.log("ist");
+    
+    next()
+   
+   
+},
+(req,res,next)=>{
+    console.log("2nd");
+    
+   next()
+},
+(req,res,next)=>{
+    console.log("3nd");
+    next()
+   
+},
+(req,res)=>{
+    console.log("4nd");
+    
+
+},
+)
+app.get('/test',(req,res)=>{
+    console.log('postman');
+    res.send('Response')
 } )
 
-app.post('/user',(req,res)=>{
+app.post('/user',(req,res)=>{   
     console.log("save data from database");
     res.send('data succesfully saved')
     
