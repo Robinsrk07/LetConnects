48c740cffc9dc5e1b726284a7a238477a19fc349
Robinsrk07 <robinsyriak07@gmail.com>
2024-12-17 15:22:20 +0530

    jwt sign verify

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
  const ConnectDb =  require('./config/database')
-const app = express();
+ var jwt = require('jsonwebtoken');
+ const app = express();
+var cookieParser = require('cookie-parser')
+const bcrypt =require('bcrypt')
 const User =require('./model/userSchema')
+const {validateSignUpdata}= require('./utils/validation')
 ConnectDb().then(()=>{
     console.log("database Connection succes");
     app.listen(3000,()=>{
@@ -12,12 +16,26 @@ ConnectDb().then(()=>{
 })
  
 app.use(express.json())
+app.use(cookieParser())
 
 app.post('/signup',async(req,res)=>{
  console.log(req.body);
  try{
-    const user = new User(req.body)
-    await user.save()
+    validateSignUpdata(req)
+
+   const {firstName,lastName,emailId,password} = req.body
+
+   const passwordHash = await bcrypt.hash(password,10)
+   console.log(passwordHash);
+
+   const user = new User({
+    firstName,
+    lastName,
+    emailId,
+    password:passwordHash
+  })
+   
+    await user.save()  
     res.send("data recieved")
  }catch(err){
     res.send('error occured :' +err.message)
@@ -25,6 +43,60 @@ app.post('/signup',async(req,res)=>{
 })
 
 
+app.post('/login',async(req,res)=>{
+try{
+const {emailId,password}=req.body;
+const user = await User.findOne({emailId:emailId})
+
+if(!user){
+    throw new Error("invalid credentials");
+}
+
+const isPasswordValid = await bcrypt.compare(password,user.password)
+
+if(isPasswordValid){
+
+const token = await jwt.sign({_id:user._id},'ROBIN@123')
+
+console.log(token);
+
+
+ 
+    res.cookie("token",token) 
+    res.send(" its a valid user")
+}else{
+    res.send("invalid credentials")
+}
+
+}catch(err){
+console.log("error occured"+err.message);
+res.status(400).send("invalid credentials")
+}
+})
+
+app.get('/profile',async(req,res)=>{
+    try{
+    const cookie = req.cookies;
+    console.log(cookie);
+    const{token} =cookie
+    if(!token){
+        throw new Error("invalid token")
+    }
+    const decode= await jwt.verify(token,"ROBIN@123");
+    console.log(decode);
+    const {_id} = decode
+    console.log("My logged in user is :"+_id);
+    const user = await User.findById(_id)
+    if(!user){
+        throw new Error("invalid user")
+    }
+    res.send(user)
+    }catch(err){
+        res.status(400).send("ERROR:"+err.message)
+    }
+})
+
+ 
 app.get('/user',async(req,res)=>{
     const userEmail = req.body.emailId
     console.log("userEmail:",userEmail);
@@ -65,7 +137,7 @@ app.patch('/user', async (req, res) => {
         const user = await User.findByIdAndUpdate(
             { _id: userId },  
             data,              
-            { new: true, runValidators: true } // Return the updated document and run validators
+            { new: true, runValidators: true } 
         );
 
         
@@ -73,7 +145,7 @@ app.patch('/user', async (req, res) => {
             return res.status(404).send("User not found");
         }
 
-        // Send updated user response
+        
         res.status(200).json(user);
 
     } catch (err) {
---
+++ src/utils/validation.js
@@ -0,0 +1,19 @@
+
+  const validator = require('validator')
+const validateSignUpdata=(req)=>{
+    const {firstName,lastName,emailId,password} = req.body;
+
+
+      if(!firstName||!lastName){
+        throw new Error(" Name is not valid")
+      }else if(!validator.isEmail(emailId)){
+        throw new Error(" enter a valid email")
+      }else if(!validator.isStrongPassword(password)){
+        throw new Error('please enter a strong password')
+      }
+}
+
+
+module.exports = {
+    validateSignUpdata
+}
